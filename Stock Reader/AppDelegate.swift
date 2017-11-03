b494674c1b0db014d3c4187acfb2ac67d6230bf9
James Stafford <jwstafford1991@gmail.com>
2017-11-03 12:03:03 -0500

    Initial Commit

---
+++ Stock Reader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Stock Reader
+//
+//  Created by James Stafford on 11/3/17.
+//  Copyright © 2017 James Stafford. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Stock Reader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Stock Reader
+//
+//  Created by James Stafford on 11/3/17.
+//  Copyright © 2017 James Stafford. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
